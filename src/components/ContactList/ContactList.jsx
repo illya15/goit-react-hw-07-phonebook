d59321df1eb160723fd53efe3ed6d8d5b5cbb188
illya15 <113913094+illya15@users.noreply.github.com>
2023-06-18 21:54:58 +0300

    hw7 9

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,41 +1,3 @@
-// import { useDispatch, useSelector } from 'react-redux';
-// import css from './ContactList.module.css';
-// import { deleteContacts } from 'redux/contactsSlice';
-
-// export const ContactList = () => {
-//   const {
-//     contacts: { contacts },
-//     filter,
-//   } = useSelector(state => state);
-
-//   const dispatch = useDispatch();
-//   const filterName = contacts.filter(contact =>
-//     contact.name.toLowerCase().includes(filter.toLowerCase())
-//   );
-
-//   const deleteContact = id => {
-//     dispatch(deleteContacts(id));
-//   };
-
-//   return (
-//     <ul children={css.list}>
-//       {filterName.map(contact => (
-//         <li className={css.item} key={contact.id}>
-//           <p>
-//             {contact.name}: {contact.number}
-//           </p>
-//           <button
-//             className={css.button}
-//             type="button"
-//             onClick={() => deleteContact(contact.id)}
-//           >
-//             Delete
-//           </button>
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
 
 
 import { useDispatch, useSelector } from 'react-redux';
@@ -43,9 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import css from './ContactList.module.css';
 import { getContactsSelector } from 'redux/selector';
 import { deleteContactsThunk } from 'redux/metods';
-// import toast, { Toaster } from 'react-hot-toast';
-// const notify = () =>
-//   toast.success('Your contact has been successfully deleted');
+
 export const ContactList = () => {
   const {
     contacts: { contacts },
@@ -59,7 +19,7 @@ export const ContactList = () => {
   );
 
   const deleteContact = id => {
-    // notify();
+    
     dispatch(deleteContactsThunk(id));
   };
 
@@ -84,7 +44,7 @@ export const ContactList = () => {
           </button>
         </li>
       ))}
-      {/* <Toaster /> */}
+      
     </ul>
   );
 };
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,95 +1,7 @@
-// import { useState } from 'react';
-// import css from './Form.module.css';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { setContacts } from 'redux/contactsSlice';
-// import { nanoid } from 'nanoid';
-
-// export const Form = () => {
-//   const [name, setName] = useState('');
-//   const [number, setNumber] = useState('');
-//   const {
-//     contacts: { contacts },
-//   } = useSelector(state => state);
-
-//   const dispatch = useDispatch();
-
-//   const handleChange = ({ target: { name, value } }) => {
-//     switch (name) {
-//       case 'name':
-//         setName(value);
-//         break;
-//       case 'number':
-//         setNumber(value);
-//         break;
-//       default:
-//         break;
-//     }
-//   };
-
-//   const addContact = e => {
-//     e.preventDefault();
-//     const duplicate = contacts.find(
-//       contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
-//     );
-
-//     if (duplicate) {
-//       alert('A customer already exists');
-//       return;
-//     }
-
-//     dispatch(setContacts({ id: nanoid(), name, number }));
-//     reset();
-//   };
-
-//   const reset = () => {
-//     setName('');
-//     setNumber('');
-//   };
-
-//   return (
-//     <form className={css.form} onSubmit={addContact}>
-//       <label>
-//         Name
-//         <br />
-//         <input
-//           type="text"
-//           name="name"
-//           pattern="^[A-Za-z\u0080-\uFFFF ']+$"
-//            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//           required
-//           onChange={handleChange}
-//           value={name}
-//         />
-//       </label>
-//       <br />
-//       <label>
-//         Number <br />
-//         <input
-//           type="tel"
-//           name="number"
-//           pattern="^(\+?[0-9.\(\)\-\s]*)$"
-//           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//           required
-//           onChange={handleChange}
-//           value={number}
-//         />
-//       </label>
-//       <br />
-//       <button type="submit">Add contact</button>
-//     </form>
-//   );
-// };
-
-
-// import { FormStyle } from './Form.styled';
 import { useDispatch, useSelector } from 'react-redux';
 import css from './Form.module.css';
 import { postContactsThunk } from 'redux/metods';
 import { getContactsSelector } from 'redux/selector';
-
-// import toast, { Toaster } from 'react-hot-toast';
-
-// const notify = () => toast.success('Your contact has been successfully added');
 
 export const Form = () => {
   const dispatch = useDispatch();
@@ -113,7 +25,7 @@ export const Form = () => {
       return;
     }
     dispatch(postContactsThunk(newContact));
-    // notify();
+
     e.target.reset();
   };
 
@@ -125,7 +37,7 @@ export const Form = () => {
         <input
           type="text"
           name="name"
-          pattern="^(\+?[0-9.\(\)\-\s]*)$"
+          pattern="^[A-Za-z\u0080-\uFFFF ']+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
@@ -145,8 +57,6 @@ export const Form = () => {
       <button type="submit" disabled={isAdding}>
         {isAdding ? 'Adding...' : 'Add contact'}
       </button>
-      {/* <Toaster /> */}
     </form>
   );
 };
-
